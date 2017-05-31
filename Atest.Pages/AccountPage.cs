b687278d728008a359042e9cc5c1717afc155268
oleksiy.bozhko <oleksiy.bozhko@caspio.com>
2017-05-31 14:50:40 +0300

    replace page factory

--- Atest.Pages/AccountPage.cs
+++ Atest.Pages/AccountPage.cs
@@ -1,6 +1,16 @@
-﻿namespace Atest.Pages
+﻿using OpenQA.Selenium;
+
+namespace Atest.Pages
 {
     public class AccountPage
     {
+        private readonly string PageUrl = "http:////www.phptravels.net//login";
+        private string PageTitle = "Login";
+        private IWebDriver _driver;
+
+        public AccountPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
     }
 }
--- Atest.Pages/LoginPage.cs
+++ Atest.Pages/LoginPage.cs
@@ -1,38 +1,44 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
 namespace Atest.Pages
 {
     public class LoginPage 
     {
-        [FindsBy(How = How.Name, Using = "username")]
-        protected IWebElement userEmail;
-
-        [FindsBy(How = How.Name, Using = "password")]
-        protected IWebElement userPassword;
-
-        [FindsBy(How = How.Id, Using = "remember-me")]
-        protected IWebElement rememberMeCheckBox;
-
-        [FindsBy(How = How.CssSelector, Using = "#loginfrm > div.wow.fadeIn.animated > button")]
-        protected IWebElement loginButton;
-
+        #region Private
         private readonly string PageUrl = "http:////www.phptravels.net//login";
-        private  string PageTitle = "Login";
+        private string PageTitle = "Login";
         private IWebDriver _driver;
+        #endregion Private
 
-        public string Title
-        {
-            get { return PageTitle; }
-        }       
-        
+        #region Constructors
         public LoginPage(IWebDriver driver)
         {
             _driver = driver;
-            _driver.Navigate().GoToUrl(PageUrl);            
+            _driver.Navigate().GoToUrl(PageUrl);
+            Assert.AreEqual(PageTitle, _driver.Title);
         }
-        
-                
+        #endregion Constructors
+
+        #region Properties
+        public string Title
+        {
+            get { return PageTitle; }
+        }
+        #endregion Properties
+
+        #region Elements
+        public IWebElement userEmail{ get { return _driver.FindElement(By.Name("username")); }} 
+
+        public IWebElement userPassword { get { return _driver.FindElement(By.Name("password")); } }
+
+        public IWebElement rememberMeCheckBox { get { return _driver.FindElement(By.Id("remember-me")); } }
+
+        public IWebElement loginButton { get { return _driver.FindElement(By.CssSelector("#loginfrm > div.wow.fadeIn.animated > button")); } }
+
+        #endregion Elements
+                 
         public AccountPage LoginAs(string email, string password, bool remember = false)
         {
             userEmail.SendKeys(email);
@@ -40,7 +46,7 @@ namespace Atest.Pages
             if (remember) { rememberMeCheckBox.Click(); }
             loginButton.Click();
 
-            return new AccountPage();
+            return new AccountPage(_driver);
            
         }
         
